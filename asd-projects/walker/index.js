0f29768a0f411a5813ab0950bb9101e761115564
OP-Bright <rowanbrite@gmail.com>
2023-06-14 20:49:11 +0000

    finished walker with bonus run feature

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -10,14 +10,24 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
-  // Game Item Objects
+  var KEY = {
+    "w": 87,
+    "s": 83,
+    "a": 65,
+    "d": 68,
+    "shift": 16
+  }
+  var posistionX = 0;
+  var posistionY = 0;
+  var speedX = 0;
+  var speedY = 0
 
+  // Game Item Objects
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
-
+  $(document).on("keydown", handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+  $(document).on("keyup", handleKeyUp);    
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -27,15 +37,55 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
-
+    moveWalker();
+    redrawWalker();
   }
   
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
-
+  function handleKeyDown (event) {
+      if (event.shiftKey === true) {
+        if (event.which === KEY.w) {
+          speedY = -12 
+        }
+        if (event.which === KEY.s) {
+          speedY = 12 
+        }
+        if (event.which === KEY.a) {
+          speedX = -12 
+        }
+        if (event.which === KEY.d) {
+          speedX = 12 
+        }
+      } else {
+        if (event.which === KEY.w) {
+          speedY = -7 
+        }
+        if (event.which === KEY.s) {
+          speedY = 7 
+        }
+        if (event.which === KEY.a) {
+          speedX = -7 
+        }
+        if (event.which === KEY.d) {
+          speedX = 7 
+        }
+      }
+  }
+  function handleKeyUp (event) {
+    if (event.which === KEY.w) {
+      speedY = 0
+    }
+    if (event.which === KEY.s) {
+      speedY = 0
+    }
+    if (event.which === KEY.a) {
+      speedX = 0
+    }
+    if (event.which === KEY.d) {
+      speedX = 0
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
@@ -49,6 +99,15 @@ function runProgram(){
 
     // turn off event handlers
     $(document).off();
+  }
+
+  function moveWalker(){
+    posistionX += speedX;
+    posistionY += speedY;
+  }
+  function redrawWalker(){
+    $("#walker").css("left", posistionX)
+    $("#walker").css("top", posistionY)
   }
   
 }
