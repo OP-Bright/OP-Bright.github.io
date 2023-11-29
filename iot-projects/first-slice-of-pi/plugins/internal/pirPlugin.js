392daa70d83ebf2a264fd910b362a47e72b0e953
OP-Bright <rowanbrite@gmail.com>
2023-11-28 19:53:35 -0600

    hopefully first slice is good

--- iot-projects/first-slice-of-pi/plugins/internal/pirPlugin.js
+++ iot-projects/first-slice-of-pi/plugins/internal/pirPlugin.js
@@ -4,3 +4,25 @@ const Gpio = require('onoff').Gpio;
 let sensor;
 const device = resources.pi.sensors.pir;
 
+function connectHardware () {
+	sensor = new Gpio(device.gpio, 'in', 'both')
+	sensor.watch(function (error, value) {
+	if (error) {
+		
+	}
+	else {
+		device.value = !!value;
+	}
+	}
+		);
+};
+
+function start () {
+	connectHardware();
+	};
+exports.start = start;
+
+function stop () {
+	sensor.unexport()
+	};
+exports.stop = stop;
