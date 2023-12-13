dcf2c647b564c4859551b5fcc3438d61b0214d13
OP-Bright <rowanbrite@gmail.com>
2023-12-12 19:54:16 -0600

    WIP TO-DO 1 pi-inyourface

--- iot-projects/pi-inyourface/plugins/internal/ledsPlugin.js
+++ iot-projects/pi-inyourface/plugins/internal/ledsPlugin.js
@@ -4,6 +4,8 @@ let actuator1, actuator2;
 let model = resources.pi.actuators.leds;
 let pluginName = resources.pi.actuators.leds[1].name + ", " + resources.pi.actuators.leds[2].name;
 
+function connectHardware () {};
+
 exports.start = function (params) {
 	connectHardware();
 	console.log("starting " + pluginName + " plugin");
