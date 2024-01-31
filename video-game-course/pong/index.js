02d79854a6f84489a33d21268354885d6e6c1ca5
OP-Bright <rowanbrite@gmail.com>
2024-01-31 01:59:58 +0000

    Restarting TODO 3/4 in its entirety. Ball jitters when it clips and the code is becoming something wild to try and fix.

--- video-game-course/pong/index.js
+++ video-game-course/pong/index.js
@@ -112,6 +112,7 @@
 
     // TODO 3: bounce the ball off each of the paddles
 
+    //Is it touching the CPU paddle ? If so bounce.
     if (ball.x + 20 >= paddleCPU.x) {
       if (ball.y <= paddleCPU.y + 100 && ball.y >= paddleCPU.y ) {
         ball.xVelocity = -ball.xVelocity - 0.5
@@ -119,12 +120,12 @@
       }
     }
 
-    if (ball.x - 20 <= paddlePlayer.x + 20 ) {
-      if (ball.y <= paddlePlayer.y + 100 && ball.y >= paddlePlayer.y ) {
-        ball.xVelocity = -ball.xVelocity + 0.5
-      }
-    }
-    
+    //Is it touching the players paddle? If so bounce.
+
+
+    //Unaccounted for fringe cases: If the top or bottom of the paddle collides with the ball.
+
+    //RESET THE GAME - Accounted for fringe case of it not bouncing but that should never ever happen
     if (ball.x <= -40 || ball.x >= canvas.width + 40 || ball.y >= canvas.height + 40 || ball.y <= 0 -40) {
       ball.x = canvas.width / 2;
       ball.y = canvas.height / 2;
